491eda7d7a8e6bad1665f5d4732567b779963fd2
Christian Parpart <christian@parpart.family>
2022-01-03 13:49:49 +0100

    [LSP] Introduce HandlerError(id, code, message) exception.

--- libsolidity/lsp/Transport.h
+++ libsolidity/lsp/Transport.h
@@ -47,6 +47,28 @@ enum class ErrorCode
 	RequestFailed = -32803
 };
 
+// Error exception used to bail out on errors in the LSP function-call handlers.
+class HandlerError: public util::Exception
+{
+public:
+	HandlerError(MessageID _id, ErrorCode _code, std::string _message):
+		m_id{_id},
+		m_code{_code},
+		m_message{_message}
+	{
+	}
+
+	MessageID id() const noexcept { return m_id; }
+	ErrorCode code() const noexcept { return m_code; }
+	std::string const& message() const noexcept { return m_message; }
+    char const* what() const noexcept override { return m_message.c_str(); }
+
+private:
+	MessageID m_id;
+	ErrorCode m_code;
+	std::string m_message;
+};
+
 /**
  * Error exception used to bail out on errors in the LSP function-call handlers.
  */
